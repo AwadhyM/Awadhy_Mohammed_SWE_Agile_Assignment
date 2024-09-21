cbb9738b52eda0d15421ed1c55998d2f8fd93ff9
Awadhy Mohammed <awadhym@outlook.com>
2024-09-21 16:28:06 +0100

    Render homepage html through use of a django template
    Set up base and home template to minimise repetition
    of code

--- django_project/knowledge_bank/views.py
+++ django_project/knowledge_bank/views.py
@@ -2,6 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+articles = [
+    {
+        "author": "Awadhy Mohammed",
+        "role": "Apprentice Software Engineer",
+        "content": "First Article",
+        "date": "October 11 2024",
+    },
+    {
+        "author": "Awadhy Mohammed",
+        "role": "Apprentice Software Engineer",
+        "content": "Second Article",
+        "date": "October 12 2024",
+    },
+]
+
+
 def home(request):
     """Routes user to the home page where all records will be displayed"""
-    return HttpResponse("<h1> Knowledge Bank Home </h1>")
+    content = {"articles": articles}
+
+    # Load html for home page
+    return render(request, "knowledge_bank/home.html", content)
