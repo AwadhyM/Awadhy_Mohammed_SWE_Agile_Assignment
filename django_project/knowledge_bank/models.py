a3e2a6c17dd805d83335ea9a5131fa30cf711a0c
Awadhy Mohammed <awadhym@outlook.com>
2024-09-21 21:57:30 +0100

    Create Article model that represents article table.
    Sample data is created, migrated and displayed on home page.

--- django_project/knowledge_bank/models.py
+++ django_project/knowledge_bank/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Article(models.Model):
+    """Article represents the article table in our database.
+    This is the data that users will be able to add, update and read.
+    """
+
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE
+    )  # on_delete argument allows post to be deleted if a user is deleted but not vice versa.
+
+    def __str__(self):
+        """Displays model instance attributes in human readable form"""
+        return f"{self.title} by {self.author} published on {self.date_posted} and last_modified {self.last_modified}"
--- django_project/knowledge_bank/views.py
+++ django_project/knowledge_bank/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-articles = [
-    {
-        "author": "Awadhy Mohammed",
-        "role": "Apprentice Software Engineer",
-        "content": "First Article",
-        "date": "October 11 2024",
-    },
-    {
-        "author": "Awadhy Mohammed",
-        "role": "Apprentice Software Engineer",
-        "content": "Second Article",
-        "date": "October 12 2024",
-    },
-]
+from .models import Article
 
 
 def home(request):
     """Routes user to the home page where all records will be displayed"""
-    content = {"articles": articles}
+    content = {"articles": Article.objects.all()}
 
     # Load html for home page
     return render(request, "knowledge_bank/home.html", content)
