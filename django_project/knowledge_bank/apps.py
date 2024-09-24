27e4936c951969a05ebf83f6c7647ad5c982dba8
Awadhy Mohammed <awadhym@outlook.com>
2024-09-24 11:17:28 +0100

    Removal of unused files

--- django_project/knowledge_bank/apps.py
+++ django_project/knowledge_bank/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class KnowledgeBankConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "knowledge_bank"
-
-    def ready(self):
-        import knowledge_bank.signals
