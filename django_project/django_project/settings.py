cb0858ca27f98c386ca0fe087ff8d33048e027fd
Awadhy Mohammed <awadhym@outlook.com>
2024-09-26 20:07:54 +0100

    Changes that are required for hosting

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import dj_database_url
+import os
 from pathlib import Path
 
 
@@ -21,12 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-lc0!4=_$y)fp3i086b&)=p*&@k5&86ac9fmixbg!p$#(k+i#7c"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Environ variable is named DEBUB and returns false by default.
+# If its true then it equals TRUE
+DEBUG = os.environ.get("DEBUG", "False") == "true"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -86,6 +90,9 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
 
 
 # Password validation
